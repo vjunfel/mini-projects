b77dbee724682e5348dd1ca50ebf607533939902
vjunfel <vjunfel@gmail.com>
2023-02-28 22:40:41 +0800

    Added spendtracker JS

---
+++ spendTracker.js
@@ -0,0 +1,81 @@
+const myForm = document.querySelector('#my-form');
+const myField = document.querySelector('#my-field');
+const myList = document.querySelector('#my-list');
+const btnDelete = document.querySelector('.btnDelete');
+const clearAll = document.querySelector('#clear');
+
+// Event Listener
+myForm.addEventListener('submit', createItem);
+myList.addEventListener('click', removeItemList);
+clearAll.addEventListener('click', clearLists);
+
+function createItem(e) {
+	e.preventDefault();
+
+	const inputValue = myField.value.trim();
+
+	if (inputValue.length !== 0) {
+		const li = document.createElement('li');
+		const button = createBtn('btnDelete');
+
+		li.appendChild(document.createTextNode(inputValue));
+		li.appendChild(button);
+		myList.appendChild(li);
+	} else {
+		const warningMessage = document.querySelector('#warning');
+		warningMessage.classList.add('warn');
+
+		setTimeout(() => {
+			warningMessage.classList.remove('warn');
+		}, 2000);
+	}
+
+	myField.value = '';
+	myField.focus();
+	checkUI();
+}
+
+function createBtn(classes) {
+	const button = document.createElement('button');
+	button.className = classes;
+
+	const icon = createIcon('fa fa-xmark');
+	button.appendChild(icon);
+	return button;
+}
+
+function createIcon(classes) {
+	const icon = document.createElement('i');
+	icon.className = classes;
+	return icon;
+}
+
+function removeItemList(e) {
+	if (e.target.parentElement.classList.contains('btnDelete')) {
+		e.target.parentElement.parentElement.remove();
+	}
+
+	checkUI();
+}
+
+function clearLists() {
+	while (myList.firstChild) {
+		myList.firstChild.remove();
+	}
+
+	checkUI();
+}
+
+function checkUI() {
+	const filter = document.querySelector('.filter-area');
+	const clear = document.querySelector('.clear-items');
+	const items = myList.querySelectorAll('li');
+
+	if (items.length === 0) {
+		filter.classList.add('hide');
+		clear.style.display = 'none';
+	} else {
+		filter.classList.remove('hide');
+		clear.style.display = 'block';
+	}
+}
